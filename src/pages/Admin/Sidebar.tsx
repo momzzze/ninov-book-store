637e0b39565edf5277554c93f1afb8c775ce7d1d
nikola.ninov <momzzze@abv.bg>
2024-03-24 16:56:32 +0200

    sidebar fix

--- src/pages/Admin/Sidebar.tsx
+++ src/pages/Admin/Sidebar.tsx
@@ -4,36 +4,52 @@ import {
   FaBookMedical,
   FaEdit,
   FaBackspace,
+  FaUserSecret,
 } from "react-icons/fa";
 import { MdOutlineManageHistory } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 import { useState } from "react";
-import logo from '../../assets/logo-icon.png';
+import logo from "../../assets/logo-icon.png";
+import { GiDramaMasks } from "react-icons/gi";
+import { TbTruckDelivery } from "react-icons/tb";
 
 const Sidebar = ({ children }: { children: React.ReactNode }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   const menuItems = [
     { name: "Dashboard", icon: <FaTh />, path: "/admin-dashboard" },
-    {
-      name: "Add Book",
-      icon: <FaBookMedical />,
-      path: "/admin-dashboard/upload",
-    },
     {
       name: "Manage",
       icon: <MdOutlineManageHistory />,
       path: "/admin-dashboard/manage",
     },
     {
-      name: "Edit Book",
-      icon: <FaEdit />,
-      path: "/admin-dashboard/edit-books/:id",
+      name: "Add Book",
+      icon: <FaBookMedical />,
+      path: "/admin-dashboard/upload",
+    },
+    {
+      name: "Add Shipper",
+      icon: <TbTruckDelivery />,
+      path: "/admin-dashboard/add-shipper",
+    },
+    {
+      name: "Add Genre",
+      icon: <GiDramaMasks />,
+      path: "/admin-dashboard/add-genre",
+    },
+    {
+      name: "Add Author",
+      icon: <FaUserSecret />,
+      path: "/admin-dashboard/add-author",
     },
     { name: "Back", icon: <FaBackspace />, path: "/" },
   ];
+  
+
   return (
     <div className="admin__container">
       <div
@@ -41,8 +57,14 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
         className="admin__sidebar"
       >
         <div className="top_section">
-          <img src={logo} style={{ display: isOpen ? "block" : "none" }} className="logo"/>         
-         
+          {window.innerWidth > 400 ? (
+            <img
+              src={logo}
+              style={{ display: isOpen ? "block" : "none" }}
+              className="logo"
+            />
+          ) : null}
+
           <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
             <FaBars onClick={toggle} />
           </div>
@@ -55,8 +77,13 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
               "link " + (isActive ? "admin_active" : "")
             }
           >
-            <div  className="admin_icon">{item.icon}</div>
-            <div style={{ display: isOpen ? "block" : "none" }} className="link_text">{item.name}</div>
+            <div className="admin_icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
           </NavLink>
         ))}
       </div>
