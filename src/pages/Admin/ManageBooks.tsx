61e4cf96f9bb125122c214c1dbcef6e27c87f569
nikola.ninov <momzzze@abv.bg>
2024-03-24 17:00:37 +0200

    add testing phase of mange books

--- src/pages/Admin/ManageBooks.tsx
+++ src/pages/Admin/ManageBooks.tsx
@@ -1,11 +1,52 @@
-import './Dashboard.css';
+import { useState } from "react";
+import "./Dashboard.css";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 
 const ManageBooks = () => {
+  const [books, setBooks] = useState([
+    { id: 1, title: "Book 1", author: "Author 1", year: 2020 },
+    { id: 2, title: "Book 2", author: "Author 2", year: 2019 },
+    { id: 3, title: "Book 3", author: "Author 3", year: 2018 },
+  ]);
+  const handleEdit = (id: string) => {
+    console.log(`Edit book with id ${id}`);
+  };
+  const handleDelete = (id: number) => {
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  };
   return (
     <div className="container__dashboard">
-        ManageBooks
+      <h2>Manage Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Year</th>
+            <th>Manage</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={book.id}>
+              <td>{book.id}</td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.year}</td>
+              <td className="manage">
+                <button onClick={() => handleEdit(book.id)}><FaEdit/> Edit</button>
+                <button className="delete" onClick={() => handleDelete(book.id)}><MdDelete/> Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default ManageBooks
+export default ManageBooks;
