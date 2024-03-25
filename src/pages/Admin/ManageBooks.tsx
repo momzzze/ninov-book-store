c8563e6e00c9216f7600f94f9692fb891f1d65dd
nikola.ninov <momzzze@abv.bg>
2024-03-25 22:58:23 +0200

    add realtime data

--- src/pages/Admin/ManageBooks.tsx
+++ src/pages/Admin/ManageBooks.tsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Dashboard.css";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-
+import { useDispatch, useSelector } from "react-redux";
+import { deleteBook, getBooksFromApi } from "../../services/book.services";
+import { AuthState, setBooks } from "../../state";
+import { AuthorWithId, BookWithId, PopulatedBook } from "../../types/types";
+import { Link } from "react-router-dom";
 
 const ManageBooks = () => {
-  const [books, setBooks] = useState([
-    { id: "1", title: "Book 1", author: "Author 1", year: 2020 },
-    { id: "2", title: "Book 2", author: "Author 2", year: 2019 },
-    { id: "3", title: "Book 3", author: "Author 3", year: 2018 },
-  ]);
-  const handleEdit = (id: string) => {
-    console.log(`Edit book with id ${id}`);
-  };
-  const handleDelete = (id: string) => {
-    const updatedBooks = books.filter((book) => book.id !== id);
-    setBooks(updatedBooks);
+  const [books, setBooksLocal] = useState<BookWithId[]>([]);
+  const dispatch = useDispatch();
+  const reduxBooks = useSelector((state: AuthState) => state.books);
+
+  useEffect(() => {
+    setBooksLocal(reduxBooks);
+  }, [reduxBooks]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      try {
+        const response = await getBooksFromApi();
+        dispatch(setBooks({ books: response }));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getBooks();
+  }, [dispatch]);
+
+  const handleDelete =async (id: string) => {
+    dispatch(setBooks({ books: books.filter((book) => book._id !== id) }))
+    const repsonse=await deleteBook(id);
+    console.log(repsonse);
   };
   return (
     <div className="container__dashboard">
@@ -31,18 +48,33 @@ const ManageBooks = () => {
           </tr>
         </thead>
         <tbody>
-          {books.map((book) => (
-            <tr key={book.id}>
-              <td>{book.id}</td>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              <td>{book.year}</td>
-              <td className="manage">
-                <button onClick={() => handleEdit(book.id)}><FaEdit/> Edit</button>
-                <button className="delete" onClick={() => handleDelete(book.id)}><MdDelete/> Delete</button>
-              </td>
-            </tr>
-          ))}
+          {books.length > 0 &&
+            books?.map((book: (BookWithId | PopulatedBook)) => (
+              <tr key={book._id}>
+                <td>{book._id}</td>
+                <td>{book.title}</td>
+                <td>
+                {(book.author as AuthorWithId)?.firstName} {(book.author as AuthorWithId)?.lastName}
+                </td>
+                <td>
+                  {new Date(book.publishedDate).toISOString().split("T")[0]}
+                </td>
+                <td className="manage">
+                  <Link
+                    className="edit-link"
+                    to={`/admin-dashboard/edit-books/${book._id}`}                    
+                  >
+                    <FaEdit /> Edit
+                  </Link>
+                  <button
+                    className="delete"
+                    onClick={() => handleDelete(book._id)}
+                  >
+                    <MdDelete /> Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
